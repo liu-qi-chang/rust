9d356255570ab5e84ab718c31c2ac15d631c0d07
liuChang <2984839626@qq.com>
2022-11-05 19:18:20 +0800

    添加drop

--- src/jest.rs
+++ src/jest.rs
@@ -1,42 +1,27 @@
 mod lib;
 
-use std::{fs, io};
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
-use futures::AsyncWriteExt;
+use futures::{AsyncReadExt, AsyncWriteExt};
 use crate::lib::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    listener.set_nonblocking(false).expect("Cannot set non-blocking");
+
     let pools = ThreadPool::new(5);
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
-    //
-    //     pools.execute(|| {
-    //         handle_connection(stream);
-    //     });
-    // }
-    for stream in listener.incoming() {
-        match stream {
-            Ok(s) => {
-                // do something with the TcpStream
-                println!("OK");
-                handle_connection(s);
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                // Decide if we should exit
-                //break;
-                // Decide if we should try to accept a connection again
-                println!("Err");
-                continue;
-            }
-            Err(e) => panic!("encountered IO error: {}", e),
-        }
+    //let reqNum = 0;
+    for stream in listener.incoming(){
+        pools.execute(|| {
+            handle_connection(stream);
+        });
+        break
     }
+
+    drop(pools)
 }
 
 fn handle_connection(mut stream: TcpStream) {
